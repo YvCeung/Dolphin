1c9a18c9e7dcf1022f9334e219327ef0d6111cf7
zhangyu <zytuatua@163.com>
2024-07-25 00:24:37 +0800

    add:网关模块创建

---
+++ dolphin-gateway/src/main/java/com/zyg/dolphin/gateway/DolphinGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zyg.dolphin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolphinGatewayApplication.class, args);
+    }
+
+}
