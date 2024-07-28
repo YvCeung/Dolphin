e73f99aac2e1078a145cb6b1ed0602593a5bc2bf
zhangyu <zytuatua@163.com>
2024-07-28 17:34:05 +0800

    fix:解决项目启动时nacos注册名有误的问题

--- dolphin-gateway/src/main/java/com/zyg/dolphin/gateway/DolphinGatewayApplication.java
+++ dolphin-gateway/src/main/java/com/zyg/dolphin/gateway/DolphinGatewayApplication.java
@@ -3,8 +3,9 @@ package com.zyg.dolphin.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.zyg.dolphin.gateway")
 public class DolphinGatewayApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DolphinGatewayApplication.class, args);
